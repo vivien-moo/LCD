fcb579530db4404c929d457a674036fdec02faca
vivien <1210717511@qq.com>
2019-05-30 19:43:32 +0800

    my code

--- main/main.js
+++ main/main.js
@@ -1,4 +1,40 @@
-module.exports = function main() {
+module.exports = function main(num) {
+	var a = [['._.','|.|','|_|'],
+	['...','..|','..|'],
+	['._.','._|','|_.'],
+	['._.','._|','._|'],
+	['...','|_|','..|'],
+	['._.','|_.','._|'],
+	['._.','|_.','|_|'],
+	['._.','..|','..|'],
+	['._.','|_|','|_|'],
+	['._.','|_|','..|']];
+	
     console.log("Debug Info");
-    return 'Hello World!';
+	var s = [];
+	for(var i =0;i<num.length;i++){
+		var m = parseInt(num[i]);
+		s.push(a[m]);
+	}
+	
+	var changes=[];
+	for(var i =0;i<s[0].length;i++){
+		
+			changes[i]=[];
+		
+	}
+	for(var i =0;i<s[0].length;i++){
+		for(var j= 0;j<s.length;j++){
+			changes[i].push(s[j][i]);
+		}		
+	}
+
+    var str1= [];		
+	for(var i=0;i<changes.length;i++){
+		str1.push(changes[i].join(' '));	
+	}	
+	
+	var str2;
+	str2 = String(str1[0]+'\n'+str1[1]+'\n'+str1[2]+'\n');
+	return str2;
 };
